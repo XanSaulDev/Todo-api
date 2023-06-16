43c965f9bcb14023edca2b210518d45687fb89e1
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-16 14:24:48 -0600

    adding todo model

--- apps/TODOS/models.py
+++ apps/TODOS/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-
+from apps.accounts.models import User
 # Create your models here.
 
 class Todo(models.Model):
-  title = models.CharField(max_length=50)
+  id_owner = models.ForeignKey(User,on_delete=models.CASCADE)
+  title = models.CharField(max_length=50,blank=False)
   content = models.CharField(max_length=250)
-  
+  is_completed = models.BooleanField(default=False)
+
+  def __str__(self):
+    return f'{self.title} {self.id_owner}'
