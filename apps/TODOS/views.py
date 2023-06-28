a2707b816590be2ab651f50eaf98a9621022b015
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-28 01:19:16 -0600

    adding pagination todos app

--- apps/TODOS/views.py
+++ apps/TODOS/views.py
@@ -3,22 +3,33 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from .models import Todo
 from .serializers import TodoSerializer,TodoSerializerCreate
 
 class TodoView(APIView):
   permission_classes = [IsAuthenticated]
-
+  todos_per_page = 10
+  
   def get(self,request,*args,**kwargs):
     user = request.user
-    todos = Todo.objects.filter(id_owner=user)
-    todos_serialized = TodoSerializer(todos,many=True)
+
+    todos = Todo.objects.filter(id_owner=user.id)
+
+    paginator = Paginator(todos,self.todos_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    todos_serialized = TodoSerializer(page_obj.object_list,many=True)
+
     return Response(
-    {
-      'ok':True,
-      'todos': todos_serialized.data
-    },
+      {
+        'count': paginator.count,
+        'num_pages': paginator.num_pages,
+        'current_page': page_obj.number,
+        'todos': todos_serialized.data,
+      },
     status.HTTP_200_OK
     )
   
@@ -79,18 +90,37 @@ class TodoView(APIView):
   
 
 class SearchTodo(APIView):
-  
-  def get(self,request,*args,**kwargs):
+  todos_per_page = 10
 
-    todos = Todo.objects.filter(  Q(title__icontains=request.data.get('search')) | 
-                                  Q(detail__icontains=request.data.get('search')))
+  def post(self,request,*args,**kwargs):
+    user = request.user
 
-    data_serialized = TodoSerializer(todos,many=True)
+    if request.data.get('search'):
+      
+      todos = Todo.objects.filter(  Q(title__icontains=request.data.get('search')) | 
+                                    Q(detail__icontains=request.data.get('search'))).filter(id_owner=user.id)
 
+      data_serialized = TodoSerializer(todos,many=True)
+
+      return Response(
+        {
+        'ok': True,
+        'todos': data_serialized.data
+        },
+        status.HTTP_302_FOUND
+      )
+    
+    todos = Todo.objects.filter(id_owner=user.id)
+    paginator = Paginator(todos,self.todos_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    todos_serialized = TodoSerializer(page_obj.object_list,many=True)
     return Response(
       {
-      'ok': True,
-      'todos': data_serialized.data
+        'count': paginator.count,
+        'num_pages': paginator.num_pages,
+        'current_page': page_obj.number,
+        'todos': todos_serialized.data,
       },
-      status.HTTP_302_FOUND
+    status.HTTP_200_OK
     )
