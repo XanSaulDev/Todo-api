0ec8ea551650ce32b2159508e04ba40201167b40
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-28 23:02:53 -0600

    WIP conf

--- Config/settings.py
+++ Config/settings.py
@@ -138,8 +138,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS=[STATIC_URL]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
