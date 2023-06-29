858e043ba49992b4d1269f258c100b4cfac5d417
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-28 22:06:00 -0600

    adding .netlify.app

--- Config/settings.py
+++ Config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app','localhost']
+ALLOWED_HOSTS = ['.vercel.app','localhost','.netlify.app']
 
 
 # Application definition
@@ -137,7 +137,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
