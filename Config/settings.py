d153ec19f0a2c34c462c7a14d064c27716625422
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-28 23:11:17 -0600

    WIP statics

--- Config/settings.py
+++ Config/settings.py
@@ -138,9 +138,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS=[STATIC_URL]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR/'static']
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
