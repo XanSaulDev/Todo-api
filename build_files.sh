8391e0c18bb30184c82421873d746d191b69eed1
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-06-29 00:04:42 -0600

    WIP conf

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,2 @@
 pip install -r requirements.txt
-python3.9 manage.py makemigrations accounts
-python3.9 manage.py makemigrations TODOS
-python3.9 manage.py migrate --fake
 python3.9 manage.py collectstatic
