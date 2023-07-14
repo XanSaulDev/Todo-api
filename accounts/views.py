45d6c7d42535ec3f51d5d5a09120134eb17cd395
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2023-07-14 17:29:00 -0600

    WIP

--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,14 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated 
 
 from .serializers import UserSerializerRegister,UserSerializer
-from .models import User
 
 # TODO: Refactor duplicate code
 
 class UserView(APIView):
-  
+  permission_classes = [IsAuthenticated] 
   def get(self,request,*args,**kwargs):
     user = request.user
     if user.is_anonymous:
